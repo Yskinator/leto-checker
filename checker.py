cc0dd734140118f80b6e739f17e4972fccba44df
Yskinator <yski2moreletters@gmail.com>
2017-10-01 23:07:42 +0300

    Made Selenium load Artemis' Umbrella.

--- checker.py
+++ checker.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import time
+from selenium import webdriver
+
+print("Opening browser")
+driver = webdriver.Chrome()
+print("Opening Artemis' Umbrella")
+driver.get("https://info.artemisumbrella.com")
+print("Looking for 'Artemi' to see if the page loaded correctly")
+if ("Artemi" in driver.page_source):
+    print("Page contains 'Artemi'")
+else:
+    print("Page doesn't contain 'Artemi'")
+print("Closing browser")
+driver.quit()
