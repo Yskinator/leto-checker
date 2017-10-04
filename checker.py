db2cd42bd2be9833f9a1b021ede276918484060c
Yskinator <yski2moreletters@gmail.com>
2017-10-04 05:57:35 +0300

    Made the script check that the staging page loads. Refactored the code into something sensible.

--- checker.py
+++ checker.py
@@ -22,51 +22,61 @@ def sendemail(from_addr, to_addr,
     server.quit()
 
 
-problems = sendemail(from_addr    = os.environ["GMAIL_USERNAME"], 
-                    to_addr = os.environ["EMAIL_RECEIVER"],
-                    subject      = 'Mail from python', 
-                    message      = 'A thing has happened. Do another thing.', 
-                    login        = os.environ["GMAIL_USERNAME"], 
-                    password     = os.environ["GMAIL_PASSWORD"])
-print(problems)
+def send_alert():
+    problems = sendemail(from_addr = os.environ["GMAIL_USERNAME"], 
+                        to_addr    = os.environ["EMAIL_RECEIVER"],
+                        subject    = 'Website down', 
+                        message    = 'Fix it!', 
+                        login      = os.environ["GMAIL_USERNAME"], 
+                        password   = os.environ["GMAIL_PASSWORD"])
 
+def check_status():
+    print("Opening browser")
+    if 'HEROKU' in os.environ:
+        chrome_options = Options()
+        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        driver=webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+    else:
+        driver = webdriver.Chrome()
+    print("Opening Artemis' Umbrella")
+    driver.get("https://staging.artemisumbrella.com")
+    print("Looking for 'Artemi' to see if the page loaded correctly")
+    if ("Artemi" in driver.page_source):
+        print("Page loaded correctly.")
+    else:
+        print("Page did not load correctly.")
+    print("Closing browser")
+    driver.quit()
 
-#print("Attempting to connect to database")
-#urlparse.uses_netloc.append("postgres")
-#url = urlparse.urlparse(os.environ["DATABASE_URL"])
-#print(url)
-#conn = psycopg2.connect(
-#    database=url.path[1:],
-#    user=url.username,
-#    password=url.password,
-#    host=url.hostname,
-#    port=url.port
-#)
-#print("Opened database successfully")
-#cur = conn.cursor()
-#print("Checking previous status")
-#cur.execute("SELECT worked FROM previous_status WHERE id=1")
-#print("Previous status: " + str(cur.fetchone())) 
-#print("Closing connection")
-#cur.close()
-#conn.close()
+def check_prev_status():
+    print("Attempting to connect to database")
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+       port=url.port
+    )
+    print("Opened database successfully")
+    cur = conn.cursor()
+    print("Checking previous status")
+    cur.execute("SELECT worked FROM previous_status WHERE id=1")
+    status = str(cur.fetchone())
+    print("Previous status: " + status) 
+    if status == "t":
+        print("Uh oh, it stopped working. Updating status")
+        cur.execute("UPDATE previous_status SET worked=FALSE WHERE id = 1;")
+        print("Committing changes")
+        conn.commit()
+        print("Sending alert email.")
+        send_alert()
+    print("Closing connection")
+    cur.close()
+    conn.close()
 
+check_status()
 
-#print("Opening browser")
-#if 'HEROKU' in os.environ:
-#    chrome_options = Options()
-#    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-#    chrome_options.add_argument('--disable-gpu')
-#    chrome_options.add_argument('--no-sandbox')
-#    driver=webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), #chrome_options=chrome_options)
-#else:
-#    driver = webdriver.Chrome()
-#print("Opening Artemis' Umbrella")
-#driver.get("https://info.artemisumbrella.com")
-#print("Looking for 'Artemi' to see if the page loaded correctly")
-#if ("Artemi" in driver.page_source):
-#    print("Page contains 'Artemi'")
-#else:
-#    print("Page doesn't contain 'Artemi'")
-#print("Closing browser")
-#driver.quit()
