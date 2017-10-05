46d024675973ae1bfa83c12325aac49383de3e4f
Yskinator <yski2moreletters@gmail.com>
2017-10-05 14:35:08 +0300

    Added a small script to check the current status of the script.

---
+++ get_status.py
@@ -0,0 +1,23 @@
+import psycopg2
+import urlparse
+import os
+
+print("Attempting to connect to database")
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+print(url)
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+print("Opened database successfully")
+cur = conn.cursor()
+print("Fetching status")
+cur.execute("SELECT * FROM previous_status;")
+print(cur.fetchone())
+print("Closing connection")
+cur.close()
+conn.close()
