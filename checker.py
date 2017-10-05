ceef95a1674da4230424c462beb323804f5fff75
Yskinator <yski2moreletters@gmail.com>
2017-10-05 13:48:14 +0300

    Fixed a print.

--- checker.py
+++ checker.py
@@ -198,7 +198,7 @@ def increment_test_counter():
     print("Fetching previous count")
     cur.execute("SELECT since_last_full_test FROM previous_status WHERE id=1;")
     count = int(cur.fetchone()[0])
-    print("Count: " + count)
+    print("Count: " + str(count))
     print("Increasing counter by one")
     count = count + 1
     cur.execute("UPDATE previous_status SET since_last_full_test = "+str(count)+" WHERE id=1;")
